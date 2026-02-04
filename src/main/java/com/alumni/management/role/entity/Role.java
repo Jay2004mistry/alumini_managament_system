1d59dc60bd5a115141cec4dc25df1fe3a6a229a6
Jay2004mistry <jaymistry6565@gmail.com>
2026-02-04 17:11:50 +0530

    Add Role entity

---
+++ src/main/java/com/alumni/management/role/entity/Role.java
@@ -0,0 +1,56 @@
+package com.alumni.management.role.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+	
+
+ // Constructors. We created it because JPA need it
+//   JPA needs an empty object to load data from the database.
+    public Role() { 
+    	
+    } 
+    
+    
+    
+//    This constructor is for US (developers), not for JPA.
+//    It helps us create objects easily and cleanly.
+    public Role(String roleName) 
+    { 
+    		this.roleName = roleName; 
+    }
+    
+    
+    
+//  Getter and Setter 
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+    
+}
