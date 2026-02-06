63c2f3edaa9aa3db0eae14573c38436408fbf2e9
Jay2004mistry <jaymistry6565@gmail.com>
2026-02-06 16:31:59 +0530

    user crud complete

--- src/main/java/com/alumni/management/user/controller/UserController.java
+++ src/main/java/com/alumni/management/user/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,12 @@ public class UserController {
 	public String deleteUser(@PathVariable Long id) {
 		userService.deleteUser(id);
 		return "Delete Successfully";
+	}
+	
+	@PutMapping("/{id}")
+	public User updateUserData(@PathVariable Long id, @RequestBody User user) {
+			return userService.updateUserData(id,user);
+		
 	}
 	
 }
--- src/main/java/com/alumni/management/user/service/UserService.java
+++ src/main/java/com/alumni/management/user/service/UserService.java
@@ -57,4 +57,26 @@ public class UserService {
 		}
 		userRepository.deleteById(id);
 	}
+
+
+	public User updateUserData(Long id, User user) {
+		User existingUser=userRepository.findById(id).
+		orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found"));
+		
+		existingUser.setName(user.getName());
+		existingUser.setEmail(user.getEmail());
+		existingUser.setPassword(user.getPassword());
+		
+		// check role exist if yes then ready to update
+	    if (user.getRole() != null) {
+	        Role role = roleRepository
+	                .findByRoleName(user.getRole().getRoleName())
+	                .orElseThrow(() ->
+	                    new ResponseStatusException(HttpStatus.NOT_FOUND, "Role not found"));
+	        existingUser.setRole(role);
+	    }
+	    
+	    return userRepository.save(existingUser);
+		
+	}
 }
